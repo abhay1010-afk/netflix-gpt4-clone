ac98a6cf592bd9303e228b31936ed50db1b3e635
abhay1010-afk <abhaytomar1010@gmail.com>
2025-05-19 12:06:29 +0530

    Your commit message here

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className='bg-rose-400 w-full h-full'>
-      <h1 className='text-4xl text-center text-red-600'>HYE WE ARE OUR FIRST PROJECT NETFLIX GPT</h1>
+      <h1 className='text-4xl text-center text-red-600'>HYE WE ARE BUILDING OUR FIRST PROJECT NETFLIX GPT</h1>
     </div>
   )
 }
